b0f288d71b3956446ba5262e6343522ccb542e39
dbfan <ershuai.fan@xuesongwan.com>
2018-11-28 13:16:17 +0800

    add  lable  href

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,15 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
 
-
     public static void searchBaiDu(String searchStr) {
         System.setProperty("webdriver.chrome.driver", "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.baidu.com/");
-        driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.findElement(By.className("s_ipt")).sendKeys(searchStr);
         driver.findElement(By.id("su")).click();
+        String link = driver.findElement(By.xpath("//*[@id=\"1\"]/h3/a")).getAttribute("href");//*[@id="1"]/h3/a
+        driver.get(link);
     }
 
 
