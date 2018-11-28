1e8963e99d368595005358d27ff009dc384980ac
dbfan <ershuai.fan@xuesongwan.com>
2018-11-28 10:48:04 +0800

    changed search String

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,18 @@ public class Main {
 
 
 
-    public static void searchBaiDu() {
+    public static void searchBaiDu(String searchStr) {
         System.setProperty("webdriver.chrome.driver", "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.baidu.com/");
         driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
-        driver.findElement(By.className("s_ipt")).sendKeys("年后再来");
+        driver.findElement(By.className("s_ipt")).sendKeys(searchStr);
         driver.findElement(By.id("su")).click();
     }
 
 
     public static void main(String[] args) {
-        searchBaiDu();
+        searchBaiDu("自动化测试");
 
     }
 }
